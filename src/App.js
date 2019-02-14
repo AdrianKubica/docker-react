01a9a4fb0ea8f214091dc5da4f666671cb429182
AdrianKubica <adrian.kubica.ak@gmail.com>
2019-02-15 00:30:25 +0100

    Changed app text

--- src/App.js
+++ src/App.js
@@ -9,9 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hi there Adrian! How u doing?
-            Yet antoher testing :)
-            Test GitHub!
+            Hi there Adrian!
           </p>
           <a
             className="App-link"
