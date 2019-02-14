767dcffa6be169e87cd820f9f268c63a78fe74e9
AdrianKubica <adrian.kubica.ak@gmail.com>
2019-02-14 22:57:34 +0100

    Yet another slack testing notification

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Hi there Adrian! How u doing?
+            Yet antoher testing :)
           </p>
           <a
             className="App-link"
